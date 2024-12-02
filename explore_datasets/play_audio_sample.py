fad2c4beda4ca8b50e9d1210889084bbfbbace9e
Wojciech Mroczyński <mroczynskiwojciech@gmail.com>
2024-12-02 23:31:33 +0100

    feat: add ability to save sample to .wav file, add printing ground truth label for audio sample

--- explore_datasets/play_audio_sample.py
+++ explore_datasets/play_audio_sample.py
@@ -1,21 +1,41 @@
+# Run in Interactive Window to display interactive media player
+
+import os
 from datasets import load_dataset
 from IPython.display import Audio, display
+import soundfile as sf
 
 
-def play_sample_audio(dataset_path, subdir, split, index):
+def extract_audio_sample_metadata(dataset_path, subdir, split, index):
     dataset = load_dataset(path=dataset_path, name=subdir)
     split = dataset[split]
     record = split[index]
+    ref_orig = record["ref_orig"]
     audio_array = record["audio"]["array"]
     sampling_rate = record["audio"]["sampling_rate"]
-    return audio_array, sampling_rate
+    return audio_array, sampling_rate, ref_orig
+
+
+def play_audio_sample(audio_array, sampling_rate):
+    display(Audio(data=audio_array, rate=sampling_rate))
+
+
+def save_audio_sample(audio_array, sampling_rate, file_path):
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    sf.write(file=file_path, data=audio_array, samplerate=sampling_rate)
 
 
 if __name__ == "__main__":
-    audio_array, sampling_rate = play_sample_audio(
+    audio_array, sampling_rate, ref_orig = extract_audio_sample_metadata(
         dataset_path="amu-cai/pl-asr-bigos-v2",
         subdir="pwr-azon_spont-20",
         split="train",
-        index=1,
+        index=0,
     )
-    display(Audio(data=audio_array, rate=sampling_rate))
+    play_audio_sample(audio_array=audio_array, sampling_rate=sampling_rate)
+    save_audio_sample(
+        audio_array=audio_array,
+        sampling_rate=sampling_rate,
+        file_path=os.path.join("audio_out", "sample.wav"),
+    )
+    print(f"Ground truth:\n{ref_orig}")
