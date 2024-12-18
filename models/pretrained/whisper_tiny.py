fc4ce5c3d2f05e05f6c4f138f0440f449581559d
Wojciech Mroczyński <mroczynskiwojciech@gmail.com>
2024-12-18 18:03:40 +0100

    feat: transcribe sample from amu-cai/pl-asr-bigos-v2 using openai/whisper-tiny model (#5)

---
+++ models/pretrained/whisper_tiny.py
@@ -0,0 +1,59 @@
+import os
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from datasets import load_dataset
+import torch
+
+
+def plot_mel_spectrogram(input_features: torch.Tensor) -> None:
+    """Visualize input audio representation for Whisper."""
+    import matplotlib.pyplot as plt
+
+    mel_spectrogram = input_features[0].numpy()
+    plt.figure(figsize=(12, 6))
+    plt.imshow(mel_spectrogram, aspect="auto", origin="lower", cmap="viridis")
+    plt.colorbar(label="Amplitude")
+    plt.xlabel("Time Steps")
+    plt.ylabel("Mel Frequency Bands")
+    plt.title("Mel-Spectrogram")
+    plt.show()
+
+
+processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
+model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+model.config.forced_decoder_ids = processor.get_decoder_prompt_ids(
+    language="polish", task="transcribe"
+)
+print(f"Special tokens:\n{processor.batch_decode(model.config.forced_decoder_ids)}")
+
+ds = load_dataset(
+    path="amu-cai/pl-asr-bigos-v2",
+    name="pwr-azon_spont-20",
+    split="train",
+)
+sample = ds[0]["audio"]
+input_features = processor(
+    sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt"
+).input_features
+plot_mel_spectrogram(input_features)
+
+predicted_ids = model.generate(input_features)
+transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+
+def clean_transcription(transcription: str) -> str:
+    """Lowercase, remove punctuation and trim whitespace."""
+    import string
+
+    return (
+        transcription.lower()
+        .translate(str.maketrans("", "", string.punctuation))
+        .strip()
+    )
+
+
+cleaned_transcription = clean_transcription(transcription[0])
+print(f"Cleaned transcription:\n{cleaned_transcription}")
+
+os.makedirs("text_out", exist_ok=True)
+with open(os.path.join("text_out", "transcription.txt"), "w") as f:
+    f.write(cleaned_transcription)
